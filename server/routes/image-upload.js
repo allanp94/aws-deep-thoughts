d3020f4d3978ca20804a9cde8fcda0cc11ddfd82
allan.pirillis <allan.p94@gmail.com>
2022-01-03 10:02:07 -0600

    set up the image upload endpoint to the s3 bucket

---
+++ server/routes/image-upload.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer"); //handles the file uploading
+const AWS = require("aws-sdk");
+
+const paramsConfig = require("../utils/params-config");
+
+const storage = multer.memoryStorage({
+  destination: function (req, file, callback) {
+    callback(null, "");
+  },
+});
+
+const upload = multer({ storage }).single("image");
+
+const s3 = new AWS.S3({
+  apiVersion: "2006-03-01",
+});
+
+router.post("/image-upload", upload, (req, res) => {
+  console.log("post('/api/image-upload'", req.file);
+
+  //params config
+  const params = paramsConfig(req.file);
+
+  //service call to s3 bucket
+  s3.upload(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
+    res.json(data);
+  });
+});
+
+module.exports = router;
---
+++ server/utils/params-config.js
@@ -0,0 +1,16 @@
+const { v4: uuidv4 } = require("uuid");
+
+const params = (fileName) => {
+  const myFile = fileName.originalname.split(".");
+  const fileType = myFile[myFile.length - 1];
+
+  const imageParams = {
+    Bucket: "user-images-026a152d-fb10-4dba-9b15-7e49c308067a",
+    Key: `${uuidv4()}.${fileType}`,
+    Body: fileName.buffer,
+  };
+
+  return imageParams;
+};
+
+module.exports = params;
